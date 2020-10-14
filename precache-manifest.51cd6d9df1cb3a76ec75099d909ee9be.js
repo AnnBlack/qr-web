76b547efb3cc1818277e671fbe988932e6fe3062
Anastasia Purtova <purtova@arrival.com>
2020-10-14 15:37:25 +0200

    Updates

---
+++ precache-manifest.51cd6d9df1cb3a76ec75099d909ee9be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5b56f920396c0a09c60e7f84e158ff",
+    "url": "/qr-web/index.html"
+  },
+  {
+    "revision": "a018ba079fada45cc563",
+    "url": "/qr-web/static/js/2.37665060.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qr-web/static/js/2.37665060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b0817a6c4f512c554e",
+    "url": "/qr-web/static/js/main.803c73e8.chunk.js"
+  },
+  {
+    "revision": "571d2a909060f29479a1",
+    "url": "/qr-web/static/js/runtime-main.a1653429.js"
+  }
+]);
