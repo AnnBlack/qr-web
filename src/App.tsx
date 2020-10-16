a4469b32840a4a51430e60a1c1b11fa6a7a6baee
Anastasia Purtova <purtova@arrival.com>
2020-10-16 10:26:55 +0200

    changed result type

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import styled from '@emotion/styled';
 import {css, Global} from '@emotion/core';
 import {BrowserMultiFormatReader, NotFoundException, Result} from '@zxing/library';
+import {Option} from 'fp-ts/Option';
+import {option} from 'fp-ts';
+import {pipe} from 'fp-ts/pipeable';
+import {flow} from 'fp-ts/function';
 
 //#region styled
 const globalStyles = css`
 	html,
 	body,
 	#root {
-		padding: 20px;
+		padding: 0;
 		margin: 0;
 	}
 `;
-const ResultAreaStyled = styled.div`
-	width: 529px;
-	height: 400px;
+const WrapperStyled = styled.div`
+	max-width: 400px;
+	height: 200px;
+	padding: 5px;
 `;
 const CameraAreaStyled = styled.div`
 	width: inherit;
@@ -24,11 +29,14 @@ const CameraAreaStyled = styled.div`
 	justify-content: center;
 	align-items: center;
 `;
+const ResultAreaStyled = styled.div`
+	padding: 5px;
+`;
 //#endregion
 
 export const App = React.memo(() => {
 	const [deviceId, setDeviceId] = React.useState<string>('');
-	const [result, setResult] = React.useState<Result>();
+	const [result, setResult] = React.useState<Option<Result>>(option.none);
 	const [isCameraActive, setIsCameraActive] = React.useState<boolean>(false);
 	const videoRef = React.useRef<HTMLVideoElement>(null);
 	const codeReader = React.useMemo(() => new BrowserMultiFormatReader(), []);
@@ -36,7 +44,7 @@ export const App = React.memo(() => {
 	React.useEffect(() => {
 		navigator.mediaDevices
 			.enumerateDevices()
-			.then(data => data.map(item => (item.kind === 'videoinput') ? setDeviceId(item.deviceId) : setDeviceId('')))
+			.then(data => data.map(item => (item.kind === 'videoinput' ? setDeviceId(item.deviceId) : setDeviceId(''))))
 			.catch(err => console.log(err));
 	}, []);
 
@@ -49,7 +57,7 @@ export const App = React.memo(() => {
 				setIsCameraActive(true);
 				if (result) {
 					console.log('result', result);
-					setResult(result);
+					setResult(option.some(result));
 				}
 				if (error && !(error instanceof NotFoundException)) {
 					console.error('error 1', error);
@@ -59,15 +67,34 @@ export const App = React.memo(() => {
 				console.log('error 2', error);
 			});
 	}, [codeReader, deviceId]);
+
 	const reset = React.useCallback(() => {
 		codeReader.reset();
 		setIsCameraActive(false);
 	}, [codeReader]);
+
+	const getResult = React.useMemo(
+		() =>
+			pipe(
+				result,
+				option.map(res => res.getText()),
+				option.fold(
+					() => <p>No result</p>,
+					flow(
+						option.fromNullable,
+						option.getOrElse<React.ReactNode>(() => <p>No result</p>),
+						result => <p>Result: {result}</p>,
+					),
+				),
+			),
+		[result],
+	);
+
 	return (
 		<React.Fragment>
 			<Global styles={globalStyles} />
 
-			<ResultAreaStyled>
+			<WrapperStyled>
 				{!isCameraActive ? (
 					<CameraAreaStyled>
 						<button onClick={start}>Activate Camera</button>
@@ -75,11 +102,12 @@ export const App = React.memo(() => {
 				) : (
 					''
 				)}
-				<video ref={videoRef} autoPlay width={'529'} height={'400'} />
+				<video ref={videoRef} autoPlay width={'100%'} height={'200'} />
+			</WrapperStyled>
+			<ResultAreaStyled>
+				{getResult}
+				<button onClick={reset}>Turn off Camera</button>
 			</ResultAreaStyled>
-			<p>result: {result && result.getText()}</p>
-
-			<button onClick={reset}>Turn off Camera</button>
 		</React.Fragment>
 	);
 });
